06c86e55703c022230d8722022ecbd837447f43c
CandiceCoding <candicenonomasimini@gmail.com>
2023-06-15 13:34:29 -0700

    Betty size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,12 @@
  */
 int main(void)
 {
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
 printf("Size of a char: %d byte(s)\n", sizeof(ch
 ar));
 printf("Size of an int: %d byte(s)\n", sizeof(in
