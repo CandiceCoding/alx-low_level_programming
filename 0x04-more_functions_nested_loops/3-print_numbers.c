b894c74582b94d5817e52b658fc9a662927a14c4
CandiceCoding <candicenonomasimini@gmail.com>
2023-06-22 08:32:31 -0700

    My numberc

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - print 0-9
- * return: void
+ * Return:Always 0
  */
 
 void print_numbers(void)
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 	_putchar('\n');
 }
